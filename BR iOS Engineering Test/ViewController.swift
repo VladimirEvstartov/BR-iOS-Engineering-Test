a73c61e84ea210fad75b23d1f8f00d1f70ae9919
Evstratov Vladimir <VEvstratov@mail.com>
2019-12-28 13:19:06 +0100

    Initial Commit

---
+++ BR iOS Engineering Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BR iOS Engineering Test
+//
+//  Created by Vladimir on 28.12.2019.
+//  Copyright © 2019 Vladimir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ BR iOS Engineering TestTests/BR_iOS_Engineering_TestTests.swift
@@ -0,0 +1,34 @@
+//
+//  BR_iOS_Engineering_TestTests.swift
+//  BR iOS Engineering TestTests
+//
+//  Created by Vladimir on 28.12.2019.
+//  Copyright © 2019 Vladimir. All rights reserved.
+//
+
+import XCTest
+@testable import BR_iOS_Engineering_Test
+
+class BR_iOS_Engineering_TestTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
